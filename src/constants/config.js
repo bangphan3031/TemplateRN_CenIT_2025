f64e5608974b8770e93cd7f61d03929e60c51128
Thien Long <tranthienlongdk@gmail.com>
2025-08-13 16:09:12 +0700

    Update code version

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-export const ANDROID_VERSION = 2;
-export const IOS_VERSION = 2;
-export const TEXT_ANDROID_VERSION = '1.0.2';
-export const TEXT_IOS_VERSION = '1.0.2';
+export const ANDROID_VERSION = 3;
+export const IOS_VERSION = 3;
+export const TEXT_ANDROID_VERSION = '1.2.1';
+export const TEXT_IOS_VERSION = '1.2.1';
